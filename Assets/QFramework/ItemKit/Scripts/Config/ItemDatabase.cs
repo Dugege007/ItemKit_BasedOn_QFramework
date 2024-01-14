fdedf429948e37907fc8b9c36b987ed1017befc3
Dugege007 <351080175@qq.com>
2024-01-14 15:17:47 +0800

    303. 物品搜索

--- Assets/QFramework/ItemKit/Scripts/Config/ItemDatabase.cs
+++ Assets/QFramework/ItemKit/Scripts/Config/ItemDatabase.cs
@@ -12,6 +12,7 @@ namespace QFramework
     [CreateAssetMenu(menuName = "@ItemKit/Create Item Database")]
     public class ItemDatabase : ScriptableObject
     {
+        [DisplayLabel("命名空间：")]
         public string NameSpace = "QFramework.Example";
 
         public List<ItemConfig> ItemConfigs = new List<ItemConfig>();
@@ -50,6 +51,10 @@ namespace QFramework
                 }
             }
 
+            private string mSearchKey = string.Empty;
+            // 使用 QFramework 提供的字体设置封装
+            FluentGUIStyle mHeader = FluentGUIStyle.Label().FontBold();
+
             // OnInspectorGUI 重写自定义编辑器的GUI布局和行为
             public override void OnInspectorGUI()
             {
@@ -59,31 +64,69 @@ namespace QFramework
                 // .Update() 更新序列化对象的状态，以准备显示
                 serializedObject.Update();
 
+                GUILayout.BeginVertical("box");
+
                 // 绘制除 ItemConfigs 之外的所有属性
                 // .DrawProperties() 是 QFramework 提供的，其中包含 base.OnInspectorGUI() 的功能
                 // .DrawProperties(是否绘制脚本，缩进值，忽略)
                 serializedObject.DrawProperties(true, 0, "ItemConfigs");
 
-                if (GUILayout.Button("+"))
+                if (GUILayout.Button("生成代码"))
                 {
-                    // 创建一个新的 ItemConfig 实例
-                    ItemConfig itemConfig = ItemConfig.CreateInstance<ItemConfig>();
-                    // 将新创建的 itemConfig 添加到 ItemDatabase 的资产中
-                    AssetDatabase.AddObjectToAsset(itemConfig, target);
+                    // 获取当前编辑的 ItemDatabase 实例
+                    // target 指的是次编辑器对应的脚本，也就是 ItemDatabase
+                    var itemDatabase = target as ItemDatabase;
 
-                    // 在 ItemConfigs 列表中添加一个新的元素
-                    mItemConfigs.InsertArrayElementAtIndex(mItemConfigs.arraySize);
-                    // 获取新添加的元素的序列化属性，并设置其值为新创建的 itemConfig
-                    SerializedProperty arrayElement = mItemConfigs.GetArrayElementAtIndex(mItemConfigs.arraySize - 1);
-                    arrayElement.objectReferenceValue = itemConfig;
+                    // 获取当前 ItemDatabase 脚本的文件路径，并确定生成代码的保存位置
+                    string filePath = AssetDatabase.GetAssetPath(target).GetFolderPath() + "/Items.cs";
 
-                    // 保存所有更改到资产
+                    // 使用 QFramework 中的代码生成功能
+                    // 创建一个代码作用域树，用于生成代码结构
+                    ICodeScope rootCode = new RootCode()
+                        // 添加命名空间
+                        .Using("UnityEngine")
+                        .Using("QFramework")
+                        // 空一行
+                        .EmptyLine()
+                        // 定义命名空间
+                        .Namespace(itemDatabase.NameSpace, ns =>
+                        {
+                            // 在命名空间中定义一个类
+                            ns.Class("Items", String.Empty, false, false, c =>
+                            {
+                                // 为每个 itemDB.ItemConfigs 生成一个静态字符串字段
+                                foreach (ItemConfig itemConfig in itemDatabase.ItemConfigs)
+                                {
+                                    c.Custom($"public static string {itemConfig.Key} = \"{itemConfig.Key}\";");
+                                    Debug.Log(itemConfig.Key);
+                                }
+                            });
+                        });
+
+                    // 创建或覆盖文件，并准备写入生成的代码
+                    // 使用 using 语句自动管理 StreamWriter 的生命周期。
+                    // 当离开 using 代码块的作用域时，fileWriter 的 Dispose 方法会被自动调用，确保文件资源被正确关闭。
+                    using StreamWriter fileWriter = File.CreateText(filePath);
+                    // 创建一个代码写入器，将代码作用域树转换为字符串
+                    FileCodeWriter codeWriter = new FileCodeWriter(fileWriter);
+                    // 生成代码并写入文件
+                    rootCode.Gen(codeWriter);
+
+                    // 保存所有未保存的资源更改
                     AssetDatabase.SaveAssets();
-                    // 刷新资源
+                    // 刷新 Unity 编辑器的资源数据库
                     AssetDatabase.Refresh();
-
-                    OnEnable();
                 }
+                GUILayout.EndVertical();
+
+                // 加个分隔符
+                EditorGUILayout.Separator();
+
+                GUILayout.Label("物品列表：", mHeader);
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("搜索：", GUILayout.Width(40));
+                mSearchKey = EditorGUILayout.TextField(mSearchKey);
+                GUILayout.EndHorizontal();
 
                 // 遍历 ItemConfigs 列表，为每个配置绘制编辑器界面
                 for (int i = 0; i < mItemEditors.Count; i++)
@@ -92,6 +135,11 @@ namespace QFramework
                     //SerializedProperty itemConfig = mItemConfigs.GetArrayElementAtIndex(i);
 
                     ItemEditorObj itemEditor = mItemEditors[i];
+
+                    if (!itemEditor.ItemConfig.Name.Contains(mSearchKey) && !itemEditor.ItemConfig.Key.Contains(mSearchKey))
+                    {
+                        continue;
+                    }
 
                     // 绘制背景框
                     GUILayout.BeginVertical("box");
@@ -140,51 +188,25 @@ namespace QFramework
                     GUILayout.EndVertical();
                 }
 
-                if (GUILayout.Button("Create Code"))
+                if (GUILayout.Button("添加物品"))
                 {
-                    // 获取当前编辑的 ItemDatabase 实例
-                    // target 指的是次编辑器对应的脚本，也就是 ItemDatabase
-                    var itemDatabase = target as ItemDatabase;
+                    // 创建一个新的 ItemConfig 实例
+                    ItemConfig itemConfig = ItemConfig.CreateInstance<ItemConfig>();
+                    // 将新创建的 itemConfig 添加到 ItemDatabase 的资产中
+                    AssetDatabase.AddObjectToAsset(itemConfig, target);
 
-                    // 获取当前 ItemDatabase 脚本的文件路径，并确定生成代码的保存位置
-                    string filePath = AssetDatabase.GetAssetPath(target).GetFolderPath() + "/Items.cs";
+                    // 在 ItemConfigs 列表中添加一个新的元素
+                    mItemConfigs.InsertArrayElementAtIndex(mItemConfigs.arraySize);
+                    // 获取新添加的元素的序列化属性，并设置其值为新创建的 itemConfig
+                    SerializedProperty arrayElement = mItemConfigs.GetArrayElementAtIndex(mItemConfigs.arraySize - 1);
+                    arrayElement.objectReferenceValue = itemConfig;
 
-                    // 使用 QFramework 中的代码生成功能
-                    // 创建一个代码作用域树，用于生成代码结构
-                    ICodeScope rootCode = new RootCode()
-                        // 添加命名空间
-                        .Using("UnityEngine")
-                        .Using("QFramework")
-                        // 空一行
-                        .EmptyLine()
-                        // 定义命名空间
-                        .Namespace(itemDatabase.NameSpace, ns =>
-                        {
-                            // 在命名空间中定义一个类
-                            ns.Class("Items", String.Empty, false, false, c =>
-                            {
-                                // 为每个 itemDB.ItemConfigs 生成一个静态字符串字段
-                                foreach (ItemConfig itemConfig in itemDatabase.ItemConfigs)
-                                {
-                                    c.Custom($"public static string {itemConfig.Key} = \"{itemConfig.Key}\";");
-                                    Debug.Log(itemConfig.Key);
-                                }
-                            });
-                        });
-
-                    // 创建或覆盖文件，并准备写入生成的代码
-                    // 使用 using 语句自动管理 StreamWriter 的生命周期。
-                    // 当离开 using 代码块的作用域时，fileWriter 的 Dispose 方法会被自动调用，确保文件资源被正确关闭。
-                    using StreamWriter fileWriter = File.CreateText(filePath);
-                    // 创建一个代码写入器，将代码作用域树转换为字符串
-                    FileCodeWriter codeWriter = new FileCodeWriter(fileWriter);
-                    // 生成代码并写入文件
-                    rootCode.Gen(codeWriter);
-
-                    // 保存所有未保存的资源更改
+                    // 保存所有更改到资产
                     AssetDatabase.SaveAssets();
-                    // 刷新 Unity 编辑器的资源数据库
+                    // 刷新资源
                     AssetDatabase.Refresh();
+
+                    OnEnable();
                 }
 
                 // 应用对序列化对象所做的所有修改，不支持撤销
