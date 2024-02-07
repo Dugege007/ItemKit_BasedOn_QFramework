37af7367630dd6db244e55a68a3ed2cee1b991ec
Dugege007 <351080175@qq.com>
2024-02-08 02:20:58 +0800

    506. 自定义加载 API 设计

---
+++ Assets/QFramework/ItemKit/Scripts/Framework/IItemKitLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace QFramework
+{
+    public interface IItemKitLoader
+    {
+        ItemConfigGroup LoadItemDatabase(string databaseName);
+        ItemLanguagePackage LoadLanguagePackage(string languagePackageName);
+
+        // 异步加载，用回调的方式
+        void LoadItemDatabaseAsync(string databaseName, Action<ItemConfigGroup> onLoadFinish);
+        void LoadLanguagePackageAsync(string languagePackageName, Action<ItemLanguagePackage> onLoadFinish);
+    }
+
+    public class DefaultItemKitLoader : IItemKitLoader
+    {
+        public ItemConfigGroup LoadItemDatabase(string databaseName)
+        {
+            return Resources.Load<ItemConfigGroup>(databaseName);
+        }
+
+        public ItemLanguagePackage LoadLanguagePackage(string languagePackageName)
+        {
+            return Resources.Load<ItemLanguagePackage>(languagePackageName);
+        }
+
+        public void LoadItemDatabaseAsync(string databaseName, Action<ItemConfigGroup> onLoadFinish)
+        {
+            // 最基本的异步加载方式
+            ResourceRequest request = Resources.LoadAsync<ItemConfigGroup>(databaseName);
+            request.completed += operation =>
+            {
+                onLoadFinish(request.asset as ItemConfigGroup);
+            };
+        }
+
+        public void LoadLanguagePackageAsync(string languagePackageName, Action<ItemLanguagePackage> onLoadFinish)
+        {
+            ResourceRequest request = Resources.LoadAsync<ItemLanguagePackage>(languagePackageName);
+            request.completed += operation =>
+            {
+                onLoadFinish(request.asset as ItemLanguagePackage);
+            };
+        }
+    }
+}
--- Assets/QFramework/ItemKit/Scripts/ItemKit.cs
+++ Assets/QFramework/ItemKit/Scripts/ItemKit.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ namespace QFramework
     public class ItemKit
     {
         public static IItemKitSaveAndLoader SaverAndLoader = new DefaultItemKitSaverAndLoader();
+        public static IItemKitLoader Loader = new DefaultItemKitLoader();
 
         public static Dictionary<string, SlotGroup> mSlotGroupByKey = new Dictionary<string, SlotGroup>();
         public static Dictionary<string, IItem> ItemByKey = new Dictionary<string, IItem>();
@@ -31,6 +33,29 @@ namespace QFramework
             return mSlotGroupByKey[key];
         }
 
+        public static void LoadItemDatabase(string databaseName)
+        {
+            ItemConfigGroup database = Loader.LoadItemDatabase(databaseName);
+            database.ItemConfigs.ForEach(config => AddItemConfig(config));
+        }
+
+        // 异步加载，协程方式
+        public static IEnumerator LoadItemDatabaseAsync(string databaseName)
+        {
+            bool done = false;
+
+            Loader.LoadItemDatabaseAsync(databaseName, database =>
+            {
+                foreach (var config in database.ItemConfigs)
+                    AddItemConfig(config);
+
+                done = true;
+            });
+
+            while (!done)
+                yield return null;
+        }
+
         // 当前语言
         public const string DefaultLanguage = "DefaultLanguage";
         public static string CurrentLanguage { get; private set; } = DefaultLanguage;
@@ -49,8 +74,7 @@ namespace QFramework
             {
                 CurrentLanguage = languagePackageName;
 
-                ItemLanguagePackage languagePackage = Resources.Load<ItemLanguagePackage>(languagePackageName);
-
+                ItemLanguagePackage languagePackage = Loader.LoadLanguagePackage(languagePackageName); 
                 foreach (var localItem in languagePackage.LocalItems)
                 {
                     if (ItemByKey.TryGetValue(localItem.Key, out var item))
@@ -61,10 +85,41 @@ namespace QFramework
             }
         }
 
-        public static void LoadItemDatabase(string databaseName)
+        // 异步加载语言包，协程方式
+        public static IEnumerator LoadItemLanguagePackageAsync(string languagePackageName)
         {
-            ItemConfigGroup database = Resources.Load<ItemConfigGroup>(databaseName);
-            database.ItemConfigs.ForEach(config => AddItemConfig(config));
+            bool done = false;
+
+            if (languagePackageName == DefaultLanguage)
+            {
+                CurrentLanguage = DefaultLanguage;
+                foreach (var item in ItemByKey.Values)
+                {
+                    item.LocalItem = null;
+                }
+
+                done = true;
+            }
+            else
+            {
+                CurrentLanguage = languagePackageName;
+
+                Loader.LoadLanguagePackageAsync(languagePackageName, languagePackage =>
+                {
+                    foreach (var localItem in languagePackage.LocalItems)
+                    {
+                        if (ItemByKey.TryGetValue(localItem.Key, out var item))
+                        {
+                            item.LocalItem = localItem;
+                        }
+                    }
+
+                    done = true;
+                }); 
+            }
+
+            while (!done)
+                yield return null;
         }
 
         public static void AddItemConfig(IItem itemConfig)
