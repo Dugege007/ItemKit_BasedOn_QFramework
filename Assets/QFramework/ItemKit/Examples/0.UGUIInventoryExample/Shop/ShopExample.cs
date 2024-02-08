ac0985affadd298ec7448cc82ccf1b6370ed5f28
Dugege007 <351080175@qq.com>
2024-02-09 00:32:34 +0800

    604. 出售功能

--- Assets/QFramework/ItemKit/Examples/0.UGUIInventoryExample/Shop/ShopExample.cs
+++ Assets/QFramework/ItemKit/Examples/0.UGUIInventoryExample/Shop/ShopExample.cs
@@ -3,6 +3,7 @@ using QFramework;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 // 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
 // 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
@@ -11,6 +12,13 @@ namespace QFramework.Example
     public partial class ShopExample : ViewController
     {
         public class ShopBuyItem
+        {
+            public IItem Item { get; set; }
+            public int Count { get; set; }
+            public int Price { get; set; }
+        }
+
+        public class ShopSellItem
         {
             public IItem Item { get; set; }
             public int Count { get; set; }
@@ -36,6 +44,62 @@ namespace QFramework.Example
         {
             UIShopItem.Hide();
 
+            //ShowBuyItems();
+
+            void RefreshSellItems()
+            {
+                ShopItemRoot.DestroyChildren();
+
+                HashSet<ShopSellItem> sellItems = new HashSet<ShopSellItem>();
+
+                foreach (var slotItem in ItemKit.GetSlotGroupByKey("物品栏").Slots
+                    .Where(s => s.Count > 0)
+                    .Select(s => s.Item)
+                    .ToHashSet())
+                {
+                    sellItems.Add(new ShopSellItem() { Item = slotItem, Count = 1, Price = 5 });
+                }
+
+                foreach (var sellItem in sellItems)
+                {
+                    int price = sellItem.Price;
+                    IItem item = sellItem.Item;
+
+                    UIShopItem shopItem = UIShopItem.InstantiateWithParent(ShopItemRoot);
+                    shopItem.UISlot.InitWithData(new Slot(item, sellItem.Count, ItemKit.GetSlotGroupByKey("商店")));
+                    shopItem.UISlot.Count.Hide();
+                    shopItem.Name.text = item.GetName;
+                    shopItem.Description.text = item.GetDescription;
+                    shopItem.PriceText.text = price.ToString();
+                    shopItem.BtnBuy.onClick.AddListener(() =>
+                    {
+                        ItemKit.GetSlotGroupByKey("物品栏").RemoveItem(item);
+                        Coin.Value += price;
+                        RefreshSellItems();
+                    });
+
+                    Coin.RegisterWithInitValue(coin =>
+                    {
+                        shopItem.BtnBuy.interactable = true;
+                        shopItem.PriceText.color = Color.green;
+
+                    }).UnRegisterWhenGameObjectDestroyed(shopItem.gameObject);
+
+                    shopItem.Show();
+                }
+            }
+
+            RefreshSellItems();
+        }
+
+        private void OnGUI()
+        {
+            IMGUIHelper.SetDesignResolution(640, 360);
+            GUILayout.Label("Coin: " + Coin.Value);
+        }
+
+        private void ShowBuyItems()
+        {
             List<ShopBuyItem> buyItems = new List<ShopBuyItem>()
             {
                 new ShopBuyItem() { Item = Items.item_green_sword, Count = 1, Price = 150 },
@@ -66,7 +130,7 @@ namespace QFramework.Example
                     if (coin >= price)
                     {
                         shopItem.BtnBuy.interactable = true;
-                        shopItem.PriceText.color = Color.green;
+                        shopItem.PriceText.color = Color.white;
                     }
                     else
                     {
@@ -80,10 +144,46 @@ namespace QFramework.Example
             }
         }
 
-        private void OnGUI()
+        private void ShowSellItems()
         {
-            IMGUIHelper.SetDesignResolution(640, 360);
-            GUILayout.Label("Coin: " + Coin.Value);
+            ShopItemRoot.DestroyChildren();
+
+            HashSet<ShopSellItem> sellItems = new HashSet<ShopSellItem>();
+
+            foreach (var slotItem in ItemKit.GetSlotGroupByKey("物品栏").Slots
+                .Where(s => s.Count > 0)
+                .Select(s => s.Item)
+                .ToHashSet())
+            {
+                sellItems.Add(new ShopSellItem() { Item = slotItem, Count = 1, Price = 5 });
+            }
+
+            foreach (var sellItem in sellItems)
+            {
+                int price = sellItem.Price;
+                IItem item = sellItem.Item;
+
+                UIShopItem shopItem = UIShopItem.InstantiateWithParent(ShopItemRoot);
+                shopItem.UISlot.InitWithData(new Slot(item, sellItem.Count, ItemKit.GetSlotGroupByKey("商店")));
+                shopItem.UISlot.Count.Hide();
+                shopItem.Name.text = item.GetName;
+                shopItem.Description.text = item.GetDescription;
+                shopItem.PriceText.text = price.ToString();
+                shopItem.BtnBuy.onClick.AddListener(() =>
+                {
+                    ItemKit.GetSlotGroupByKey("物品栏").RemoveItem(item);
+                    Coin.Value += price;
+                });
+
+                Coin.RegisterWithInitValue(coin =>
+                {
+                    shopItem.BtnBuy.interactable = true;
+                    shopItem.PriceText.color = Color.green;
+
+                }).UnRegisterWhenGameObjectDestroyed(shopItem.gameObject);
+
+                shopItem.Show();
+            }
         }
     }
 }
