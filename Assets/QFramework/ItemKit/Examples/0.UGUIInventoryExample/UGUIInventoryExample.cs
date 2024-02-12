c421a0606db2088dfffd29247a48173860b9962f
Dugege007 <351080175@qq.com>
2024-02-12 16:38:31 +0800

    v0.6.9 库存

--- Assets/QFramework/ItemKit/Examples/0.UGUIInventoryExample/UGUIInventoryExample.cs
+++ Assets/QFramework/ItemKit/Examples/0.UGUIInventoryExample/UGUIInventoryExample.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
-using System;
 using UnityEngine.UI;
 using System.Collections.Generic;
-using System.Linq;
 
 // 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
 // 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
