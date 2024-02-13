ea7e5a1fa07a7e924826fd93a06767c1768e86ef
Dugege007 <351080175@qq.com>
2024-02-14 00:50:54 +0800

    706. Tip 显示位置改进

--- Assets/QFramework/ItemKit/Scripts/Component/UIItemTip.cs
+++ Assets/QFramework/ItemKit/Scripts/Component/UIItemTip.cs
@@ -75,7 +75,7 @@ namespace QFramework
 
                 // 获取 TipPanel 的四个角点
                 slotRectTrans.GetWorldCorners(slotCorners);
-                //float slotWidth = slotCorners[3].x - slotCorners[0].x;
+                float slotWidth = slotCorners[3].x - slotCorners[0].x;
                 float slotHeight = slotCorners[1].y - slotCorners[0].y;
 
                 //Vector3 mousePos = Input.mousePosition;
@@ -83,23 +83,26 @@ namespace QFramework
                 Vector3[] tipCorners = new Vector3[4];
                 RectTransform tipRectTrans = mDefault.TipPanel.transform as RectTransform;
 
-                // 更新 TipPanel 内容后，强制刷新布局
-                LayoutRebuilder.ForceRebuildLayoutImmediate(tipRectTrans);
-
                 // 获取 TipPanel 的四个角点
                 tipRectTrans.GetWorldCorners(tipCorners);
                 float tipWidth = tipCorners[3].x - tipCorners[0].x;
                 float tipHeight = tipCorners[1].y - tipCorners[0].y;
 
+                float width = slotWidth + tipWidth;
+                float height = slotHeight + tipHeight;
+
+                // 更新 TipPanel 内容后，强制刷新布局
+                LayoutRebuilder.ForceRebuildLayoutImmediate(tipRectTrans);
+
                 // 根据鼠标和屏幕的相对位置，调整生成 TipPanel 的位置
                 // Slot 位置在 右下，TipPanel 生成在 左上
-                if (mousePos.y < tipHeight && mousePos.x > tipWidth)
+                if (mousePos.y < height && mousePos.x > width)
                     tipRectTrans.position = mousePos + 0.5f * tipHeight * Vector3.up + 0.5f * tipWidth * Vector3.left + 0.5f * slotHeight * Vector3.up;
                 // Slot 位置在 左上，TipPanel 生成在 右下
-                else if (mousePos.y > tipHeight && mousePos.x < tipWidth)
+                else if (mousePos.y > height && mousePos.x < width)
                     tipRectTrans.position = mousePos + 0.5f * tipHeight * Vector3.down + 0.5f * tipWidth * Vector3.right + 0.5f * slotHeight * Vector3.down;
                 // Slot 位置在 左下，TipPanel 生成在 右上
-                else if (mousePos.y < tipHeight && mousePos.x < tipWidth)
+                else if (mousePos.y < height && mousePos.x < width)
                     tipRectTrans.position = mousePos + 0.5f * tipHeight * Vector3.up + 0.5f * tipWidth * Vector3.right + 0.5f * slotHeight * Vector3.up;
                 // Slot 位置在 右上，TipPanel 生成在 左下
                 else
